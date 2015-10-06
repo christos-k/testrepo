8c33ac340ed570c9659a576b1a93c62f976cb73e
christos k <xkatsas@gmail.com>
2015-10-07 00:40:18 +0300

    Added goodbye.c
    Added evening.c

---
+++ evening.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	fprintf(stdout, "Good evening, world!");
+
+	return EXIT_FAILURE;
+}
---
+++ goodbye.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	fprintf(stderr, "Goodbye, world!");
+
+	return EXIT_SUCCESS
+}
